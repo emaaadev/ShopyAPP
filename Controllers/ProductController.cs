1d0b07254720b85dc25b395170bf195f68d2a302
Emanuel-hub-developer <emanuelbalbuena295@gmail.com>
2025-11-24 13:19:33 -0400

    feat: implement create functionality and add productform
    Implemented the logic to create and save new records.
    
    Changes included:
    - Created the backend POST endpoint to handle data submission.
    - Built the "Add New Product" form interface.
    - Added input validation to ensure data integrity.
    - Integrated the form submission with the backend API.
    - Added a success notification upon creation.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -23,6 +23,43 @@ namespace ShopyAPP.Controllers
         {
             return _context.Product.ToList();
         }
+
+
+        //Crear Producto
+        [HttpPost]
+        [Route("crearProducto")]
+        public IActionResult CrearProducto([FromBody] ProductModel nuevoProducto)
+        {
+            if (nuevoProducto == null)
+            {
+                return BadRequest("El producto no puede ser nulo.");
+            }
+
+            var product = new ProductModel
+            {
+                name = nuevoProducto.name,
+                price = nuevoProducto.price,
+                stock = nuevoProducto.stock
+        
+            };
+
+
+            try
+            {
+                _context.Product.Add(product);
+
+
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al crear el producto: {ex.Message}");
+            }
+
+            return Ok(new { success = true, message = "Producto actualizado exitosamente!", result = product });
+        }
+
     }
 
 }
